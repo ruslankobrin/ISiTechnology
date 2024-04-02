0037f388235a0a52f0ccdeea9a69bd02c4e478f1
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-04-02 18:25:07 +0300

    create messages views

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('threads/', ThreadCreateListView.as_view(), name='thread_create_list'),
     path('threads_by_user/<int:user_id>/', ThreadListAPIView.as_view(), name='threads_by_user'),
     path('threads/<int:pk>/', ThreadRetrieveDeleteView.as_view(), name='thread_retrieve_delete'),
+    path('messages/', MessageCreateListView.as_view(), name='message_create_list'),
+    path('messages/<int:pk>/', MessageRetrieveView.as_view(), name='message_retrieve'),
+    path('messages_by_thread/<int:thread_id>/', MessageListByThreadAPIView.as_view(), name='messages_by_thread'),
+    path('messages/<int:pk>/read/', MessageMarkAsReadView.as_view(), name='message_mark_as_read'),
+    path('unread/<str:user>/', UnreadMessageCountView.as_view(), name='unread_message_count'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics, serializers
+from rest_framework import generics, serializers, status
+from rest_framework.response import Response
 
-from .models import Thread
-from .serializers import ThreadSerializer
+from .models import Thread, Message
+from .serializers import ThreadSerializer, MessageSerializer
 
 
 class ThreadCreateListView(generics.ListCreateAPIView):
@@ -23,9 +24,63 @@ class ThreadRetrieveDeleteView(generics.RetrieveDestroyAPIView):
     lookup_field = "pk"
 
 
+class MessageCreateListView(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        sender = serializer.validated_data.get("sender")
+        thread = serializer.validated_data.get('thread')
+
+        if not Thread.objects.filter(id=thread.id, participants=sender).exists():
+            raise serializers.ValidationError("You are not this participant in this thread")
+        serializer.save()
+
+
+class MessageRetrieveView(generics.RetrieveAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    lookup_field = "pk"
+
+
+class MessageMarkAsReadView(generics.UpdateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    lookup_field = "pk"
+    http_method_names = ["put"]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_read = True
+        instance.save()
+        return Response(
+            {"message": "Message marked as read"}, status=status.HTTP_200_OK
+        )
+
+
+class UnreadMessageCountView(generics.GenericAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get(self, request, *args, **kwargs):
+        user = self.kwargs.get("user")
+        unread_count = Message.objects.filter(
+            thread__participants=user, is_read=False
+        ).count()
+        return Response({"unread_count": unread_count}, status=status.HTTP_200_OK)
+
+
 class ThreadListAPIView(generics.ListAPIView):
     serializer_class = ThreadSerializer
 
     def get_queryset(self):
         user_id = self.kwargs['user_id']
         return Thread.objects.filter(participants__id=user_id)
+
+
+class MessageListByThreadAPIView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        thread_id = self.kwargs['thread_id']
+        return Message.objects.filter(thread_id=thread_id)
