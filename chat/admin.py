c49f9422c0ceb97e87ac70864812b5b653e5be07
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-04-02 18:43:24 +0300

    customize django admin

--- chat/admin.py
+++ chat/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from chat.models import Thread, Message
 
-admin.site.register(Thread)
-admin.site.register(Message)
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ("id", "created", "updated")
+    list_filter = ("created", "updated")
+    search_fields = ("id",)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "sender", "thread", "created", "is_read")
+    list_filter = ("created", "is_read")
+    search_fields = ("id", "sender__username", "thread__id")
+
+
+admin.site.site_header = "Chat Admin"
+admin.site.site_title = "Chat Admin"
