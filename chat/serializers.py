bb6e8d7ec26bdd048f2ab7927682874327c02fab
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-04-02 14:02:13 +0300

    create serializers.py

---
+++ chat/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Thread, Message
+
+class ThreadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Thread
+        fields = '__all__'
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
