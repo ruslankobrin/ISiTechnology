1e26796166847abee867c057d7838ccca9929b35
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-04-02 13:35:15 +0300

    create models Thread, Message

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat.models import Thread, Message
+
+admin.site.register(Thread)
+admin.site.register(Message)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Thread(models.Model):
+    participants = models.ManyToManyField(User)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
